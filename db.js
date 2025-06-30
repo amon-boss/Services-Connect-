150bbfb77e5f9d523d869380191ab2aa4101d88e
amon-boss <66650142+amon-boss@users.noreply.github.com>
2025-06-30 00:21:50 +0000

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
@@ -6,7 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
   } catch (error) {
-    console.error("❌ MongoDB connection failed:", error);
+    console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
